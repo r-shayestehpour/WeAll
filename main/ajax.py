60ad753bdcf7a392de1efc8be9c2d6510e9f888a
Meraj Nouredini <merajnooredini@gmail.com>
2013-07-03 18:58:16 +0430

    working on newsfeed

--- main/ajax.py
+++ main/ajax.py
@@ -25,6 +25,9 @@ def new_post(request, newtext):
 def update(request):
     posts = models.Post.objects.all()[:20]
     html = ''
+    now = timezone.now()
+    time = ("%s" %now)
+    time = time[:16]
     for i in posts :
-        html += '<p>' + i.user.first_name + ' ' + i.user.last_name + ' : ' + "<br>" + i.text + "</P><hr width='33%'>"
+        html += '<p>' + i.user.first_name + ' ' + i.user.last_name + ' updated status '  + ' : ' + "<br>" + i.text + "</P>" + time + " <hr width='33%'>"
     return simplejson.dumps({'data':html})
